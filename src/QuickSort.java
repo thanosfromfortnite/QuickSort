a8614c8815b34c869021d7deb643acfac0b0ba75
BT_1N3_26 <sdfsdf>
2018-11-27 11:28:52 -0500

    sdfsdfsdf

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,27 +1,29 @@
 public class QuickSort {
-/*
-JESSE HAN
- */
+/*JESSE HAN*/
     public static void quickSort(int[] arr, int left, int right) {
         if (left < right) {
             int pivot = partition(arr, left, right);
 
             quickSort(arr, left, pivot-1);
             quickSort(arr, pivot+1, right);
+            /*JESSE HAN*/
         }
     }
+    /*JESSE HAN*/
 
     public static int partition(int[] arr, int left, int right) {
         int pivot = arr[right];
         int i = left - 1;
+        /*JESSE HAN*/
         for (int j = left; j < right; j ++) {
             if (arr[j] <= pivot) {
                 i ++;
+                // AAAAAAAAAHHHHHHHHHHHHHHHHHHHH
                 int k = arr[i];
                 arr[i] = arr[j];
                 arr[j] = k;
             }
-        }
+        } /*JESSE HAN MADE THIS AND NOT JACKIE WU OR ANYBODY ELSE*/
         int x = arr[i + 1]; arr[i + 1] = arr[right]; arr[right] = x;
         return i + 1;
     }
--- src/Runner.java
+++ src/Runner.java
@@ -1,16 +1,16 @@
 public class Runner {
-
+    /*JESSE HAN*/
     public static void main(String[] args) {
         int[] array = {3, 345, 123, 6, 234, 69, 33, 12};
 
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
         }
-
+        /*JESSE HAN MADE IT*/
         QuickSort.quickSort(array, 0, 7);
         System.out.println("Sorted!");
         for (int i = 0; i < array.length; i++) {
             System.out.println(array[i]);
-        }
+        } /*always tip your favorite halal cart*/
     }
 }
